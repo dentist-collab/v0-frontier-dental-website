017a307279e2712d89fbbb5b8c7159d98b73e179
Mushamba Dauda <angulezaabu@gmail.com>
2025-12-20 10:16:04 +0300

    fix: switch deployment config from nextjs to vite

---
+++ app/App.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+import { useMemo, useState } from "react"
+import type { Container, Theme } from "./settings/types"
+import { PortfolioNavbar } from "./components/generated/PortfolioNavbar"
+import { InnovateAIHero } from "./components/generated/InnovateAIHero"
+import { PhilosophySection } from "./components/generated/PhilosophySection"
+import { FeaturesGrid } from "./components/generated/FeaturesGrid"
+import { FeaturesSection } from "./components/generated/FeaturesSection"
+import { TestimonialsSection } from "./components/generated/TestimonialsSection"
+import { PricingSection } from "./components/generated/PricingSection"
+import { FAQSection } from "./components/generated/FAQSection"
+import { Footer } from "./components/generated/Footer"
+import { ServicesPage } from "./pages/ServicesPage"
+import { BlogPage } from "./pages/BlogPage"
+import { ContactPage } from "./pages/ContactPage"
+import { GalleryPage } from "./pages/GalleryPage"
+
+const theme: Theme = "light"
+const container: Container = "none"
+
+function App() {
+  const [currentPage, setCurrentPage] = useState<"home" | "services" | "blog" | "contact" | "gallery">("home")
+
+  function setTheme(theme: Theme) {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  }
+
+  setTheme(theme)
+
+  const generatedComponent = useMemo(() => {
+    if (currentPage === "services") {
+      return (
+        <>
+          <PortfolioNavbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+          <ServicesPage />
+          <Footer />
+        </>
+      )
+    } else if (currentPage === "blog") {
+      return (
+        <>
+          <PortfolioNavbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+          <BlogPage />
+          <Footer />
+        </>
+      )
+    } else if (currentPage === "gallery") {
+      return (
+        <>
+          <PortfolioNavbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+          <GalleryPage />
+          <Footer />
+        </>
+      )
+    } else if (currentPage === "contact") {
+      return (
+        <>
+          <PortfolioNavbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+          <ContactPage />
+          <Footer />
+        </>
+      )
+    }
+
+    // Home page (default)
+  // Home page (default)
+  return (
+    <>
+      <PortfolioNavbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <InnovateAIHero />
+      <PhilosophySection />
+      <FeaturesGrid />
+      <FeaturesSection />
+      <TestimonialsSection />
+      <PricingSection />
+      <FAQSection />
+      <Footer />
+    </>
+  )
+  }, [currentPage])
+
+  if (container === "centered") {
+    return <div className="h-full w-full flex flex-col items-center justify-center">{generatedComponent}</div>
+  } else {
+    return generatedComponent
+  }
+}
+
+export default App
